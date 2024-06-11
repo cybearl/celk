f9ab88c940fa726fa83dbfa4abeb57fe12f1eb6d
yoratoni <yoratoni.dev@gmail.com>
2024-06-11 22:04:45 +0200

    Added PostgreSQL

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,18 @@
 declare module NodeJS {
     interface ProcessEnv {
+        // Default value
+        [key: string]: string | undefined;
+
+        // Node environment
+        NODE_ENV: "development" | "production";
+
+        // PostgreSQL database
+        POSTGRES_HOST: string;
+        POSTGRES_PORT: string;
+        POSTGRES_USER: string;
+        POSTGRES_PASSWORD: string;
+        POSTGRES_DB: string;
+
         // Redis database
         REDIS_HOST: string;
         REDIS_PORT: string;
