3ce3c473f7d8dc1b44c0a66065fba55902d75dd0
yoratoni <yoratoni.dev@gmail.com>
2024-09-02 10:40:28 +0200

    WIP

--- config/redis.ts
+++ config/redis.ts
@@ -1,6 +1,9 @@
 import env from "#start/env"
 import { default as IORedis } from "ioredis"
 
+/**
+ * The configuration for the Redis database.
+ */
 const redisConfig: IORedis.RedisOptions = {
     // https://github.com/nocodb/nocodb/issues/2452#issue-1279896470
     maxRetriesPerRequest: null,
